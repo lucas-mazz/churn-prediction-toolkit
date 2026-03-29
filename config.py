849473ecd8eb344b1162a65867ccf5753e8323f7
Lucas <mzzcchll@gmail.com>
2026-03-29 02:34:04 -0300

    refactor: add PATH configuration for data to config.py

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
+import os
+
+# ----- CONFIGURACION DE PATHS
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
+RAW_DATA_PATH = os.path.join(DATA_DIR, "raw_customers.csv")
+PROCESSED_DATA_PATH = os.path.join(DATA_DIR, "clean_customers.csv")
+
 # ----- LOGICA DE NEGOCIO (CONSTANTES) -----
 
 VIP_THRESHOLD_SPEND = 1000.0    # Gasto minimo para ser VIP
